a63a76eb1ccebda9cdbae64157bff2f6572dc92f
tobias2004 <tobiiascr7@hotmail.com>
2025-04-16 00:25:30 -0700

    fix slug.astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,13 @@ import { defineCollection, z } from "astro:content";
 const projects = defineCollection({
   schema: z.object({
     name: z.string(),
-    summary: z.string().optional(),
-    githublink: z.string().url().optional(),
-    techstack: z.array(z.string()),
+    summary: z.string(),
+    githublink: z.string().optional(),
+    techstack: z.array(z.string()).optional(),
     image: z.string().optional(),
   }),
 });
 
 export const collections = {
-    projects,
-  };
-  
+  projects,
+};
